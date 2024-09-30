1d2aee7d825a51b2caff32fa6b6fbadc4a210996
MohammadRezaAlizadeh13845 <alizadehmohmmadreza1384@gmaail.com>
2024-09-30 09:04:10 +0330

    Initial commit with Python program

---
+++ Practice.py
@@ -0,0 +1,14 @@
+import msvcrt
+sen =18
+if sen <59:
+    vaziat ="ta javani az javanit lezzat bebar"
+    print ("hanooz javooni" , vaziat)
+if sen <20:
+    vaziat="hala halaha vaght da"
+    print (f"{vaziat}ri pas keife donyaro bokon")
+print ("har dokmei ro bezani kharje mishe az barname")
+msvcrt.getch()
+#this program doesn't really do something special and it is just me practicing things I learned about coding.    
+
+
+
